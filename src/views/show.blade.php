ea02880979960d8a32060945d5457e3c307928bd
Tim Schwartz <tim@metaverse.systems>
2018-05-02 03:03:05 -0500

    Changed formatting of 'show' view

--- src/views/show.blade.php
+++ src/views/show.blade.php
@@ -6,20 +6,24 @@
 
 @foreach($items as $item)
 <div class="row">
-  <div class="col s12 m6">TXID: {{{ $item['txid'] }}}</div>
-  <div class="col s12 m6">Time: {{{ $item['time'] }}}</div>
-</div>
-<div class="row">
-  <div class="col s12 m6">Key: {{{ $item['key'] }}}</div>
-  <div class="col s12 m6">Publishers:<br />
+  <div class="col s12">TXID: {{{ $item['txid'] }}}</div>
+  <div class="col s6">Time: {{{ date('Y-m-d H:i:s', $item['time']) }}}</div>
+  <div class="col s6">Key: {{{ $item['key'] }}}</div>
+  <div class="col s12">Publishers:<br />
   @foreach($item['publishers'] as $publisher)
     {{{ $publisher }}}<br />
   @endforeach
   </div>
 </div>
 <div class="row">
-  <div class="s12 m6">Size: {{{ (strlen($item['data']) / 1024) }}}K</div>
-  <div class="s12 m6">Data:<br />
+<?php 
+$size = strlen($item['data']);
+
+if($size > 2048) $size = ($size / 1024)." Kbytes";
+else $size .= " bytes";
+?>
+  <div class="s12">Size: {{{ $size }}}</div>
+  <div class="s12">Data:<br />
     {{{ hex2bin($item['data']) }}}
   </div>
 </div>
