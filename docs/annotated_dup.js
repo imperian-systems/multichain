c5f6b937b0b5e2dcd5acadaa1fb3001666dd5d99
Tim Schwartz <tim@metaverse.systems>
2018-05-08 08:57:28 -0500

    Doxygen doesn't support PHP traits. Removing.

--- docs/annotated_dup.js
+++
@@ -1,4 +0,0 @@
-var annotated_dup =
-[
-    [ "imperiansystems", "namespaceimperiansystems.html", "namespaceimperiansystems" ]
-];
--- docs/namespaceimperiansystems.js
+++
@@ -1,4 +0,0 @@
-var namespaceimperiansystems =
-[
-    [ "multichain", "namespaceimperiansystems_1_1multichain.html", "namespaceimperiansystems_1_1multichain" ]
-];
